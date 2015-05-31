a1e8d4f93a9d37dcc96e0216de32aaf5ecea1e66
Syu93 <herve.tutuaku@gmail.com>
2015-05-31 20:39:35 +0200

    [feature] add other button

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -14,11 +14,17 @@ import javax.swing.Action;
 import java.awt.Color;
 import java.awt.Font;
 import javax.swing.ImageIcon;
+import javax.swing.JSpinner;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 
 public class Gui {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
 
 	/**
 	 * Launch the application.
@@ -61,6 +67,54 @@ public class Gui {
 		btnStart.setFont(new Font("Comic Sans MS", Font.ITALIC, 11));
 		btnStart.setBounds(335, 227, 89, 23);
 		frame.getContentPane().add(btnStart);
+		
+		JButton btnStop = new JButton("Stop");
+		btnStop.setFont(new Font("Comic Sans MS", Font.ITALIC, 11));
+		btnStop.setBounds(236, 228, 89, 23);
+		frame.getContentPane().add(btnStop);
+		
+		JSpinner spinner = new JSpinner();
+		spinner.setBounds(10, 29, 105, 23);
+		frame.getContentPane().add(spinner);
+		
+		JLabel lblNumeraPasSimulation = new JLabel("Numera pas simulation");
+		lblNumeraPasSimulation.setBounds(10, 10, 115, 23);
+		frame.getContentPane().add(lblNumeraPasSimulation);
+		
+		textField = new JTextField();
+		textField.setBounds(10, 171, 45, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNouritureRaporte = new JLabel("Nouriture raport\u00E9e");
+		lblNouritureRaporte.setBounds(10, 151, 105, 23);
+		frame.getContentPane().add(lblNouritureRaporte);
+		
+		JLabel lblNouritureRestante = new JLabel("Nouriture restante");
+		lblNouritureRestante.setBounds(121, 151, 89, 23);
+		frame.getContentPane().add(lblNouritureRestante);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(124, 171, 86, 20);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblQtePheromone = new JLabel("Qte pheromone");
+		lblQtePheromone.setBounds(236, 151, 115, 23);
+		frame.getContentPane().add(lblQtePheromone);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(239, 171, 86, 20);
+		frame.getContentPane().add(textField_2);
+		
+		JLabel lblVitesseDissipation = new JLabel("Vitesse dissipation");
+		lblVitesseDissipation.setBounds(135, 10, 89, 23);
+		frame.getContentPane().add(lblVitesseDissipation);
+		
+		JSpinner spinner_1 = new JSpinner();
+		spinner_1.setBounds(131, 29, 105, 23);
+		frame.getContentPane().add(spinner_1);
 	}
 	private class SwingAction extends AbstractAction {
 		public SwingAction() {
